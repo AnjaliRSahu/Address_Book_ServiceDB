3fe1b711763cb1460e20a17d68710bf1149bfd12
AnjaliSahu <anjalisahu1996@gmail.com>
2022-10-14 15:53:51 +0530

    CompletedAllUCs

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -19,3 +19,40 @@ insert into Address_BookTable values('Anjali','Sahu','Malhotra Heights','Mumbai'
 
 update Address_BookTable
 set Address='New Heights'where FirstName='John';
+
+DELETE FROM Address_BookTable where FirstName='Smith';
+
+select * from Address_BookTable where city='Mumbai' and state='Maharashtra';
+
+select count(city)as totalrows from Address_BookTable;
+
+insert into Address_BookTable values('Aniket','Pate','Gurgaon','Mumbai','Maharashtra',78451,7894512630,'pateAni@gmail.com');
+
+select * from Address_BookTable 
+order by FirstName,city;
+
+alter table Address_BookTable
+add Type varchar(255);
+
+Update Address_BookTable
+set Type='Family' where FirstName='Anjali';
+
+
+Update Address_BookTable
+set Type='Family' where FirstName='Aniket';
+
+
+Update Address_BookTable
+set Type='Friend' where FirstName='Ash';
+
+Update Address_BookTable
+set Type='Profession' where FirstName='John';
+
+select * from Address_BookTable;
+
+select count(Type) from Address_BookTable;
+
+
+Update Address_BookTable
+set Type='Family and Friend' where FirstName='Ash';
+
