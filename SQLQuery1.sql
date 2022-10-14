20319d8e4d1a5f4964c12db28697dc8f7d5f6dc9
AnjaliSahu <anjalisahu1996@gmail.com>
2022-10-14 13:44:59 +0530

    UC2-Added Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,13 @@
 create database Address_Book_ServiceDB;
+create table Address_BookTable
+(
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+city varchar(255),
+state varchar(255),
+zip int,
+phoneNumber bigint,
+email varchar(255)
+)
+select * from Address_BookTable;
